d0bab98bfcb1773ddf792c202b8bc3d01e1cac12
Bader Ealyyah <bader.al.ealyyah@gmail.com>
2020-01-12 19:47:11 +0300

    the project hello

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def function_name(request):
+    context ={
+    'msg': 'Hello World!'
+    }
+    return render(request, 'task.html', context)
+
